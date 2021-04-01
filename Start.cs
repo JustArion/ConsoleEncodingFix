15c9ee5589a0e61420cb69ef145bbc095119617e
arion <53127594+Arion-Kun@users.noreply.github.com>
2021-04-01 13:58:32 -0200

    Add files via upload

---
+++ Start.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using static MelonLoader.MelonLogger;
+
+namespace Dawn.EncodingFix
+{
+    internal sealed class Start : MelonMod
+    {
+        public override void VRChat_OnUiManagerInit()
+        {
+            MelonPreferences.CreateCategory("EncodingFix", "Encoding Fix");
+            MelonPreferences.CreateEntry("EncodingFix", "Enabled", true);
+            MelonPreferences.CreateEntry("EncodingFix", "PageID", 0, "Advanced: SetPageID (int) Default = 0");
+            
+            LocalPrefsSaved(); // Don't wanna call OnPrefs for eveyone lol.
+        }
+        public override void OnPreferencesSaved()
+        {
+            LocalPrefsSaved();
+        }
+        private static void LocalPrefsSaved()
+        {
+            m_Enabled = MelonPreferences.GetEntryValue<bool>("EncodingFix", "Enabled");
+            m_PageID = MelonPreferences.GetEntryValue<int>("EncodingFix", "PageID");
+            if (!m_Enabled) return;
+            SetCodingPage((uint) m_PageID);
+        }
+        private static bool m_Enabled;
+        private static int m_PageID;
+        [DllImport("kernel32.dll", SetLastError = true)] // Credits to Bluescream for the DLLImport links!
+        private static extern bool SetConsoleOutputCP(uint wCodePageID);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool SetConsoleCP(uint wCodePageID);
+
+        static void SetCodingPage(uint i = 65001) // https://stackoverflow.com/questions/38533903/set-c-sharp-console-application-to-unicode-output/59307528#59307528
+        {
+            //Credits to Cillié Malan
+            SetConsoleOutputCP(i);
+            SetConsoleCP(i);
+            Console.OutputEncoding = Encoding.GetEncoding((int) i);
+            Console.InputEncoding = Encoding.GetEncoding((int) i);
+
+        }
+    }
+}
